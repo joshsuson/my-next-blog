601ee56bf172bb16c0db75181aa093f2159139b4
Josh Suson <Josh Suson>
2021-04-29 20:00:20 +0000

    Working on header

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
     <>
       <header>
         <h1 className="uppercase text-5xl text-center">
-          Josh <span className="text-green-400">Suson</span>
+          <Link href="/">
+            <a>
+              Josh <span className="text-green-400">Suson</span>
+            </a>
+          </Link>
         </h1>
+        <div>
+          <ul>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </header>
       <main className="container mx-auto">{children}</main>
     </>
