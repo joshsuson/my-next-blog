5dffba9f624a9cec2457fe90136443e9e60bcdc6
Josh Suson <joshsuson@Joshs-Mac-mini.local>
2021-05-04 16:37:58 -0400

    Finishing up jumbotron

---
+++ components/Jumbotron.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Jumbotron() {
+  return (
+    <div className="grid grid-cols-2 gap-14 mt-4">
+      <div>
+        <div className="bg-green-400">
+          <div className="relative w-auto h-96 transform -translate-x-3 -translate-y-2">
+            <Image src="/images/075A6510.jpg" layout="fill" objectFit="cover" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="w-7/12">
+          <h1 className="text-4xl py-1 px-1 text-white">
+            Hi. Nice to meet you!
+          </h1>
+          <div className="bg-green-400 w-auto h-12 relative transform -translate-y-12 -translate-x-2 -skew-x-6"></div>
+        </div>
+        <p className="text-lg leading-relaxed -mt-4">
+          My name is Josh Suson (in case you missed the title). I'm a Front End
+          Web Developer that's learning to develop applications as well. I spend
+          most of my time in Javascript. The most important thing to know about
+          me is that I'm on a mission to create products that help the world be
+          a better place through the internet.
+        </p>
+        <button className="bg-gradient-to-br from-green-300 to-green-400 uppercase text-white py-3 px-6 mt-10 green-button-shadow custom-button-hover">
+          More About Me
+        </button>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Jumbotron from "../components/Jumbotron";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home page</h1>
+      <Jumbotron />
     </Layout>
   );
 }
